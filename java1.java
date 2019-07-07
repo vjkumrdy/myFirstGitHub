bc682f9ea688f159756c0b85527dced2f5824c54
vjkumrdy <52626983+vjkumrdy@users.noreply.github.com>
2019-07-07 09:15:39 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,10 @@
+public claass Main
+{
+public static void main(String[] args)
+{
+   /**********************
+   write aprogram that adda A to B and store the result in C
+   where A =3, B = 4, C = 5
+   **********************/
+}// end of main
+}// end of class
