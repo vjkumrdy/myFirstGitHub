d7e31c1549f189e26ebaa408a274475e3172d1cd
vjkumrdy <52626983+vjkumrdy@users.noreply.github.com>
2019-07-07 09:51:57 -0400

    Update java1.java

--- java1.java
+++ java1.java
@@ -4,7 +4,10 @@ public static void main(String[] args)
 {
    /**********************
    write aprogram that adda A to B and store the result in C
-   where A =3, B = 4, C = 5
+   where A = 3, B = 5
    **********************/
+   int A = 3;
+   int B = 4;
+   int C = A+ B;
 }// end of main
 }// end of class
